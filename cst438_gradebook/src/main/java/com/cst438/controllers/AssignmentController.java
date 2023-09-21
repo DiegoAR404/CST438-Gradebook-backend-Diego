6e24b25bd00986d979776c3cee89c2e52f4f5b96
DiegoAR404 <111939314+DiegoAR404@users.noreply.github.com>
2023-09-21 10:21:27 -0700

    assignment 2

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -1,6 +1,7 @@
 package com.cst438.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -51,4 +53,45 @@ public class AssignmentController {
 	}
 	
 	// TODO create CRUD methods for Assignment
+	
+	// create a new assignment
+	@PostMapping("/assignment")
+	public AssignmentDTO[] createAssignments(@RequestBody AssignmentDTO assignmentDTO) {
+		//create a new assignment for instructor
+		// the following needs to be changed.
+		String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email) 
+		List<Assignment> assignments = assignmentRepository.findByEmail(instructorEmail);
+		AssignmentDTO[] result = new AssignmentDTO[assignments.size()];
+		for (int i=0; i<assignments.size(); i++) {
+			Assignment as = assignments.get(i);
+			AssignmentDTO dto = new AssignmentDTO(
+					as.getId(), 
+					as.getName(), 
+					as.getDueDate().toString(), 
+					as.getCourse().getTitle(), 
+					as.getCourse().getCourse_id());
+			result[i]=dto;
+		}
+		return result;
+		
+	}
+	// delete an assignment
+	//  DELETE  /course/12389
+	//  DELETE  /course/12389?force=yes
+
+	@DeleteMapping("/assignment/{id}")
+	public void deleteAssignment(@PathVariable("id") int id,  
+								 @RequestParam("force") Optional<String> force) {
+	// delete code here.
+	}
+	
+	// update assignment
+	@PutMapping("/assignment/{id}")
+	public void updateCourse(@RequestBody AssignmentDTO assignmentDTO) {
+	//implement update here
+		
+	}
+	
+
+	
 }
